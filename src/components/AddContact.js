b65281604a6867bbdcd9b86d5b611467262d7bd9
Raza Rauf <m.razarauf@gmail.com>
2022-12-30 22:41:26 -0600

    implements contact form

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -3,7 +3,20 @@ import React from "react";
 class AddContact extends React.Component {
     render () {
         return (
-            <h1>Add Contact</h1>
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" name="name" placeholder="Enter Name"></input>
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="Enter Email"></input>
+                    </div>
+                    <button className="ui button blue">Add</button>
+                </form>
+            </div>
         )
     }
 }
