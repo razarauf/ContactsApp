8e24a569e39ba4d7156803720fc9e8b4c9265417
Raza Rauf <m.razarauf@gmail.com>
2022-12-30 22:32:42 -0600

    adds skeleton code for AddContact

---
+++ src/components/AddContact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class AddContact extends React.Component {
+    render () {
+        return (
+            <h1>Add Contact</h1>
+        )
+    }
+}
+
+export default AddContact;
