a6ce36697544d6691367624f0e7985396aeee509
Raza Rauf <m.razarauf@gmail.com>
2022-12-30 23:29:49 -0600

    renders a hardcoded list passed from the App parent component into the ContactList component and displays it as a list

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,25 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+
+  const contactsArray = [
+    {
+      id: 0,
+      name: "Bob Loblaw",
+      email: "bob@gmail.com"
+    },
+    {
+      id: 1,
+      name: "Guy Fierri",
+      email: "guy@gmail.com"
+    }
+  ];
+
   return (
     <div className='ui container'>
       <Header/>
       <AddContact/>
-      <ContactList/>
+      <ContactList contactsArray={contactsArray}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-const ContactList =  () => {
+const ContactList = (props) => {
+
+    console.log(props);
+
+    const renderedContactsList = props.contactsArray.map((contact) => {
+        return (
+            <div className="item">
+                <div className="content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                    <i className="ui right floated trash alternate outline icon"></i>
+                </div>
+            </div>
+        );
+    });
+
     return (
-        <div className="ui celled list">Contact List</div>
+        <div className="ui celled list">{renderedContactsList}</div>
     )
 }
 
