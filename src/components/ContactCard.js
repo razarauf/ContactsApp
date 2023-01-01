766f4fcddd23664f83c8cb33f3bc7aba61cf430a
Raza Rauf <m.razarauf@gmail.com>
2023-01-01 02:33:32 -0600

    puts contact card html inside ContactCard

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,8 +2,15 @@ import React from "react";
 
 class ContactCard extends React.Component {
     render () {
+        const {name, email} = this.props.contact;
         return (
-            <h1>Contact Card</h1>
+            <div className="item">
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div>{email}</div>
+                    <i style={{color:"red", marginTop:"7px"}} className="ui right floated trash alternate outline icon"></i>
+                </div>
+            </div>
         )
     }
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,11 @@
 import React from "react";
+import ContactCard from "./ContactCard"
 
 const ContactList = (props) => {
 
-    console.log(props);
-
     const renderedContactsList = props.contactsArray.map((contact) => {
         return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                    <i className="ui right floated trash alternate outline icon"></i>
-                </div>
-            </div>
+            <ContactCard contact={contact}/>
         );
     });
 
