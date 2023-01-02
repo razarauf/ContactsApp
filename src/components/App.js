00c61e3047eebd8c6475e9339f3a8c748fab880c
Raza Rauf <m.razarauf@gmail.com>
2023-01-02 04:12:59 -0600

    store form in data in state in AddContact and pass it up to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from "react";
+import React, {useState} from "react";
 
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -8,24 +8,17 @@ import ContactList from "./ContactList";
 
 function App() {
 
-  const contactsArray = [
-    {
-      id: 0,
-      name: "Bob Loblaw",
-      email: "bob@gmail.com"
-    },
-    {
-      id: 1,
-      name: "Guy Fierri",
-      email: "guy@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+  }
 
   return (
     <div className='ui container'>
       <Header/>
-      <AddContact/>
-      <ContactList contactsArray={contactsArray}/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contactsArray={contacts}/>
     </div>
   );
 }
