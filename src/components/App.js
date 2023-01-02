4263fc837339eb8833c01f7c58efccc556148dfe
Raza Rauf <m.razarauf@gmail.com>
2023-01-02 05:19:11 -0600

    uses useEffect to persist contacts array locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -8,11 +8,24 @@ import ContactList from "./ContactList";
 
 function App() {
 
+  const LOCAL_STORAGE_KEY = "contacts";
+
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
-  }
+  };
+
+  useEffect(() => {
+    const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrievedContacts) {
+      setContacts(retrievedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className='ui container'>
