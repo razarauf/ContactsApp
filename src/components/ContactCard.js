13b4592e32e4d683298a3dc3baf278aa3ec744a2
Raza Rauf <m.razarauf@gmail.com>
2024-03-30 05:04:03 -0600

    Updates ContactCard to a function based component

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-class ContactCard extends React.Component {
-    render () {
-        const {name, email} = this.props.contact;
-        return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{name}</div>
-                    <div>{email}</div>
-                    <i style={{color:"red", marginTop:"7px"}} className="ui right floated trash alternate outline icon"></i>
-                </div>
+export default function ContactCard(props) {
+    const {name, email} = props.contact;
+    
+    return (
+        <div className="item">
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
+                <i style={{color:"red", marginTop:"7px"}} className="ui right floated trash alternate outline icon"></i>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default ContactCard;
