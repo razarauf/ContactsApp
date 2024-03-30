0e47bc8e8f1e135b5633024e8536a8544204cc8e
Raza Rauf <m.razarauf@gmail.com>
2024-03-30 05:33:30 -0600

    Updates AddContact to a function based component

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,52 +1,55 @@
 import React from "react";
 
-class AddContact extends React.Component {
+export default function AddContact(props) {
 
-    state = {
-        name: '',
-        email: ''
-    }
+    const [state, setState] = React.useState({name: '', email: ''})
 
-    submitHandler = (e) => {
+    function submitHandler(e) {
         e.preventDefault();
-        if (this.state.name === "" || this.state.email === "") {
+
+        if (state.name === "" || state.email === "") {
             alert("All fields are mandatory");
             return;
         }
-        this.props.addContactHandler(this.state);
-        this.setState({name: "", email: ""})
+        
+        props.addContactHandler(state);
+        setState({ name: "", email: "" })
     }
 
-    render () {
-        return (
-            <div className="ui main">
-                <h2>Add Contact</h2>
-                <form className="ui form" onSubmit={this.submitHandler}>
-                    <div className="field">
-                        <label>Name</label>
-                        <input 
-                            type="text" 
-                            name="name" 
-                            placeholder="Enter Name"
-                            value={this.state.name}
-                            onChange={(e) => this.setState({name: e.target.value})}
-                        />   
-                    </div>
-                    <div className="field">
-                        <label>Email</label>
-                        <input 
-                            type="text" 
-                            name="email" 
-                            placeholder="Enter Email"
-                            value={this.state.email}
-                            onChange={(e) => this.setState({email: e.target.value})}
-                        />
-                    </div>
-                    <button className="ui button blue">Add</button>
-                </form>
-            </div>
-        )
+    function onChangeHandler(e){
+        const {name, value} = e.target
+        setState(prevState => ({
+            ...prevState,
+            [name]: value 
+        }))
     }
+
+    return (
+        <div className="ui main">
+            <h2>Add Contact</h2>
+            <form className="ui form" onSubmit={submitHandler}>
+                <div className="field">
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter Name"
+                        value={state.name}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+                <div className="field">
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Enter Email"
+                        value={state.email}
+                        onChange={onChangeHandler}
+                    />
+                </div>
+                <button className="ui button blue">Add</button>
+            </form>
+        </div>
+    )
 }
-
-export default AddContact;
